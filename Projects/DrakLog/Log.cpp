7dddbbdf0cd89f63d3b0dbbb72bc3461cd3960c6
Benjamin McDonald <streetsamurai2001@gmail.com>
2018-03-05 15:57:21 -0500

    Set to norm

--- Projects/DrakLog/Log.cpp
+++ Projects/DrakLog/Log.cpp
@@ -6,17 +6,17 @@
 
 FILE * GetLogFile() {
 	static FILE* logFile;
-	if (!logFile)
-	{
-		std::time_t now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	if (!logFile) {
 		std::tm* time = new std::tm;
+		std::time_t now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 		localtime_s(time, &now_c);
-		std::string filename = "Logs/Log " + std::to_string(time->tm_year +1900) + "." + std::to_string(time->tm_mon)
+
+		std::string filename = "Logs/Log " + std::to_string(time->tm_year + 1900) + "." + std::to_string(time->tm_mon)
 			+ "." + std::to_string(time->tm_mday) + " " + std::to_string(time->tm_hour) + "."
 			+ std::to_string(time->tm_min) + "." + std::to_string(time->tm_sec) + ".txt";
 		delete time;
-		if (fopen_s(&logFile, filename.c_str(), "w") == 0)
-		{
+
+		if (fopen_s(&logFile, filename.c_str(), "w") == 0) {
 			return logFile;
 		}
 	}
