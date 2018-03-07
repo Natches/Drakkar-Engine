033cb7a0780bf284a84faf824b38ad40b72705c2
Benjamin McDonald <streetsamurai2001@gmail.com>
2018-03-07 11:30:05 -0500

    Modify log

--- Source/Log/Log.cpp
+++ Source/Log/Log.cpp
@@ -4,37 +4,8 @@
 #include <time.h>
 
 FILE * OpenLogFile(const char* filenameFromCategory) {
-	FILE* pLogFile = new FILE;
-	std::string filename;
-	if (filenameFromCategory == nullptr)
-	{
-		std::time_t t = time(NULL);
-		char* timeBuf = new char[100];
-		std::tm componentWiseTime;
-		localtime_s(&componentWiseTime, &t);
-		asctime_s(timeBuf, 100, &componentWiseTime);
-		std::string time(timeBuf);
-		delete[] timeBuf;
-		time.pop_back();
-		size_t posOfSpace;
-		do {
-			posOfSpace = time.find(':');
-			if (posOfSpace == std::string::npos)
-				break;
-			time[posOfSpace] = '_';
-		} while (posOfSpace != std::string::npos);
-
-		do {
-			posOfSpace = time.find(' ');
-			if (posOfSpace == std::string::npos)
-				break;
-			time[posOfSpace] = '_';
-		} while (posOfSpace != std::string::npos);
-		filename = std::string("Logs/Log_" + time + ".txt");
-	}
-	else
-		filename = std::string(filenameFromCategory);
-	if (fopen_s(&pLogFile, filename.c_str(), "w") == 0) {
+	FILE* pLogFile = nullptr;
+	if (fopen_s(&pLogFile, filenameFromCategory, "a+") == 0) {
 		return pLogFile;
 	}
 	return nullptr;
