127133a2e49b53fb2a9b3ba248fb3fa0b236efd2
Simon Gleizes <gleizes1993@gmail.com>
2018-03-22 23:05:07 -0400

    Suppressed type mismatch warnings

--- Source/Video/main.cpp
+++ Source/Video/main.cpp
@@ -22,22 +22,20 @@ void testRun(ARenderWindow* pWin) {
 	const std::vector<U16>& indices = mesh.indices();
 
 	gl::GLVertexBuffer vbo;
-	vbo.create(verts.data(), verts.size());
+	vbo.create(verts.data(), (U32)verts.size());
 
 	gl::GLIndexBuffer ibo;
-	ibo.create(indices.data(), indices.size());
+	ibo.create(indices.data(), (I32)indices.size());
 
 	gl::GLVertexArray vao;
 	vao.create(vbo, ibo);
 
 	Camera c;
-	c.lookAt({ 0.f, 0.f, -30.f }, { 0.f, 0.f, 0.f }, { 0.f, 1.f, 0.f });
+	c.lookAt({ 0.f, 0.f, -10.f }, { 0.f, 0.f, 0.f }, { 0.f, 1.f, 0.f });
 	c.perspective(60.f, 16.f / 9.f, 0.1f, 100.f);
 
-	Mat4f mvp = c.viewPerspective() * 
-		Translate<F32>({5.f, 2.f, 8.f}) *
-		Rotation({ 45.f, 25.f, 63.f }) *
-		Scale<F32>({2.f, 6.f, 3.425f});
+	Mat4f mvp = c.viewPerspective() *
+		Rotation({45.f, 45.f, 0.f});
 
 	gl::GLRHI::s_defaultShader.use();
 	gl::GLRHI::s_defaultShader.setUniform("MVP", mvp);
