28e7f5e0aef36e605f5ca98e046ba4548191f5bf
Simon Gleizes <gleizes1993@gmail.com>
2018-04-05 15:54:07 -0400

    Began supporting instanced draws

--- Source/Video/Graphics/Rendering/OpenGL/GLIndexBuffer.cpp
+++ Source/Video/Graphics/Rendering/OpenGL/GLIndexBuffer.cpp
@@ -20,9 +20,14 @@ void GLIndexBuffer::create(const GLushort* pIndices, GLsizei indexCount) {
 		GL_MAP_WRITE_BIT);
 }
 
-void GLIndexBuffer::drawElements(GLenum primType) {
+void GLIndexBuffer::drawElements() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_glID);
-	glDrawElements(primType, m_indexCount, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_TRIANGLES, m_indexCount, GL_UNSIGNED_SHORT, 0);
+}
+
+void GLIndexBuffer::drawElementsInstanced() {
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_glID);
+	glDrawElementsInstanced(GL_TRIANGLES, m_indexCount, GL_UNSIGNED_SHORT, 0, 1);
 }
 
 } // namespace gl
